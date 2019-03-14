0b5c5afd065ac6a27aadc2abc2a92031e752d838
Tomasz Borychowski <tomasz.borychowski@sap.com>
2019-03-14 15:06:14 +0000

    initial

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+	watch: true,
+	mode: 'development',
+	entry: { index: './src/index.js' },
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	resolve: { extensions: ['.js', '.json', '.html'] },
+	stats: 'normal',
+	module: {
+		rules: [
+			// { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+			{
+				test: /\.html$/,
+				exclude: /node_modules/,
+				use: { loader: 'svelte-loader' },
+			},
+		]
+	}
+};
